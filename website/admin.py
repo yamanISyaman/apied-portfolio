e2de926107d81de72e0fc691a6683bc0009508f4
Yaman <yaman.1423@gmail.com>
2024-03-14 16:29:51 +0300

    defined models initially

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from website.models import CERT, EXP, Project
 
 # Register your models here.
+for i in [CERT, EXP, Project]:
+    admin.site.register(i)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class CERT(models.Model):
+    title = models.CharField(max_length=25, blank=False)
+    image = models.ImageField(upload_to="certs/", blank=False)
+    granted_on = models.DateField()
+
+
+class EXP(models.Model):
+    title = models.CharField(max_length=25, blank=False)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Project(models.Model):
+    title = models.CharField(max_length=25, blank=False)
+    description = models.TextField()
+    image = models.ImageField(upload_to="certs/", blank=False)
+    code_url = models.URLField()
+    preview_url = models.URLField()
