0b50ff330b0f9f06997e74d1de810661d92aefae
Yaman <yaman.1423@gmail.com>
2024-03-25 14:12:53 +0300

    added cache for list and retrieve

--- backend/website/api/views.py
+++ backend/website/api/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import logout
 from rest_framework import permissions, viewsets, response, generics
 from rest_framework.decorators import api_view
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie, vary_on_headers
 from django.shortcuts import redirect
 from website.models import EXP, Project, CERT,  User
 from website.api.permissions import UserModifyOrReadOnly
@@ -15,11 +18,37 @@ class CertViewSet(viewsets.ModelViewSet):
     serializer_class = CertSerializer
     permission_classes = [UserModifyOrReadOnly]
 
+    @method_decorator(cache_page(600))
+    @method_decorator(vary_on_cookie)
+    @method_decorator(vary_on_headers("Authorization"))
+    def retrieve(self, *args, **kwargs):
+        return super(CertViewSet, self).retrieve(*args, **kwargs)
+    
+
+    @method_decorator(cache_page(600))
+    @method_decorator(vary_on_cookie)
+    @method_decorator(vary_on_headers("Authorization"))
+    def list(self, *args, **kwargs):
+        return super(CertViewSet, self).list(*args, **kwargs)
+
 
 class EXPViewSet(viewsets.ModelViewSet):
     queryset = EXP.objects.all()
     serializer_class = EXPSerializer
     permission_classes = [UserModifyOrReadOnly]
+
+    @method_decorator(cache_page(600))
+    @method_decorator(vary_on_cookie)
+    @method_decorator(vary_on_headers("Authorization"))
+    def retrieve(self, *args, **kwargs):
+        return super(EXPViewSet, self).retrieve(*args, **kwargs)
+    
+
+    @method_decorator(cache_page(600))
+    @method_decorator(vary_on_cookie)
+    @method_decorator(vary_on_headers("Authorization"))
+    def list(self, *args, **kwargs):
+        return super(EXPViewSet, self).list(*args, **kwargs)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -28,11 +57,30 @@ class ProjectViewSet(viewsets.ModelViewSet):
     permission_classes = [UserModifyOrReadOnly]
 
 
+    @method_decorator(cache_page(600))
+    @method_decorator(vary_on_cookie)
+    @method_decorator(vary_on_headers("Authorization"))
+    def retrieve(self, *args, **kwargs):
+        return super(ProjectViewSet, self).retrieve(*args, **kwargs)
+    
+
+    @method_decorator(cache_page(600))
+    @method_decorator(vary_on_cookie)
+    @method_decorator(vary_on_headers("Authorization"))
+    def list(self, *args, **kwargs):
+        return super(ProjectViewSet, self).list(*args, **kwargs)
+
+
 class UserDetail(generics.RetrieveAPIView):
+    
     lookup_field = "username"
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [UserModifyOrReadOnly]
+
+    @method_decorator(cache_page(600))
+    def retrieve(self, *args, **kwargs):
+        return super(UserDetail, self).retrieve(*args, **kwargs)
 
 
 @api_view(['GET', 'Post'])
--- backend/website/test_user_api.py
+++ backend/website/test_user_api.py
@@ -4,8 +4,10 @@ from .models import User, EXP
 
 
 class UserApiTestCase(TestCase):
-
+    # setting up the testcase environment
     def setUp(self):
+
+        # creating two users
         self.u1 = User.objects.create(username="test1")
         self.u2 = User.objects.create(username="test2")
         self.u1.set_password('password')
@@ -13,9 +15,10 @@ class UserApiTestCase(TestCase):
         self.u1.save()
         self.u2.save()
 
+        # make use of the APIClient
         self.client = APIClient()
 
-
+        # login with user 2 and making an exp object
         self.client.login(username="test2", password="password")
         exp = {
             'title': 'test title u2',
@@ -26,6 +29,7 @@ class UserApiTestCase(TestCase):
         resp = self.client.post('/api/v1/exps/', exp)
 
     
+    # testing the user detail view
     def test_users_details(self):
         resp1 = self.client.get("/api/v1/users/test1")
         resp2 = self.client.get("/api/v1/users/test2")
@@ -33,6 +37,7 @@ class UserApiTestCase(TestCase):
         self.assertEqual(resp2.json()["username"], self.u2.username)
 
     
+    # testing the creation of an object using post request
     def test_user_post(self):
 
         self.client.login(username="test1", password="password")
@@ -47,19 +52,36 @@ class UserApiTestCase(TestCase):
         self.assertEqual(exp['description'], e.description)
 
     
+    # testing the ability to put or patch another users data
     def test_put_permission(self):
         self.client.logout()
 
         self.client.login(username="test1", password="password")
 
         e = EXP.objects.get(user=self.u2, id=1)
-        print(e.title)
 
         new = {"title": "test title u1"}
 
         resp = self.client.patch("/api/v1/exps/1/", new)
 
         e = EXP.objects.get(user=self.u2, id=1)
-        print(e.title)
 
         self.assertNotEqual(new['title'], e.title)
+
+
+    # test listing objects
+    def test_object_list(self):
+
+        resp = self.client.get("/api/v1/exps/")
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.json()), 1)
+
+
+    # test the retrieval of an object
+    def test_object_retrieve(self):
+
+        resp = self.client.get("/api/v1/exps/1/")
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()['id'], 1)
