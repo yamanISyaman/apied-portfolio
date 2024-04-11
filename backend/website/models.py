218b54d73facbd0a2228a586582912053311f853
Yaman <yaman.1423@gmail.com>
2024-04-12 00:28:32 +0300

    allowed blank in model fields

--- backend/website/models.py
+++ backend/website/models.py
@@ -20,7 +20,7 @@ class CERT(models.Model):
     details_url = models.URLField(blank=True)
     donor = models.CharField(max_length=40, blank=True)
     image = models.ImageField(upload_to="certs/", blank=True)
-    granted_on = models.DateField()
+    granted_on = models.DateField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="certs")
 
 
@@ -33,16 +33,16 @@ class SKILL(models.Model):
 
 class EXP(models.Model):
     title = models.CharField(max_length=100, blank=False)
-    description = models.TextField()
-    start_date = models.DateField()
-    end_date = models.DateField()
+    description = models.TextField(blank=True)
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="exps")
 
 class Project(models.Model):
     title = models.CharField(max_length=100, blank=False)
-    description = models.TextField()
-    image = models.ImageField(upload_to="projects/", blank=False)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="projects/", blank=True)
     code_url = models.URLField(null=True, blank=True)
     preview_url = models.URLField(null=True, blank=True)
-    created_at = models.DateField(default=datetime.date.today)
+    created_at = models.DateField(default=datetime.date.today, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
